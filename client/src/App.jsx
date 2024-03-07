a9d323ca1fc655f37443bd478edeae1ca3d65279
OJ <onkarjoshi296@gmail.com>
2024-03-07 09:11:15 +0530

    Fix import statements and reformat code

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,21 +1,20 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
-import SignIn from './pages/Signin';
-import SignUp from './pages/Signup';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
 import About from './pages/About';
-import PrivateRoute from './components/PrivateRoute';
-import CreateListing from './pages/CreateListing';
 import Profile from './pages/Profile';
 import Header from './components/Header';
+import PrivateRoute from './components/PrivateRoute';
+import CreateListing from './pages/CreateListing';
 import UpdateListing from './pages/UpdateListing';
 import Listing from './pages/Listing';
 import Search from './pages/Search';
+
 export default function App() {
-  
   return (
-    
     <BrowserRouter>
-    <Header/>
+      <Header />
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/sign-in' element={<SignIn />} />
@@ -23,15 +22,15 @@ export default function App() {
         <Route path='/about' element={<About />} />
         <Route path='/search' element={<Search />} />
         <Route path='/listing/:listingId' element={<Listing />} />
-        <Route path='/profile' element={<Profile />} />
+
         <Route element={<PrivateRoute />}>
           <Route path='/profile' element={<Profile />} />
-        </Route>
-        <Route path='/create-listing' element={<CreateListing />} />
-        <Route
+          <Route path='/create-listing' element={<CreateListing />} />
+          <Route
             path='/update-listing/:listingId'
             element={<UpdateListing />}
           />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
